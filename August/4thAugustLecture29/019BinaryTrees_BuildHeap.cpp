5b393dadb3fcbdf5475f6e32130da0e0cbcae2a9
abhishek18124 <abhishek18124@iiitd.ac.in>
2021-08-05 02:55:01 +0530

    Lecture 29 BuildHeap Code Update

--- August/4thAugustLecture29/019BinaryTrees_BuildHeap.cpp
+++ August/4thAugustLecture29/019BinaryTrees_BuildHeap.cpp
@@ -1,6 +1,6 @@
 /*
 
-Given an array of n integers, design a O(n) algorirthm to build a max heap.
+Given an array of n integers, design a O(n) algorirthm to build a min heap.
 
 */
 
@@ -8,14 +8,54 @@ Given an array of n integers, design a O(n) algorirthm to build a max heap.
 
 using namespace std;
 
+void heapify(int* arr, int n, int i) {
+	
+	int m = i;
+	int l = 2*i;
+	int r = 2*i + 1;
+
+	// uncomment lines [19-21] and comment lines [23-25] for max heap
+
+	// if(l < n && arr[l] > arr[m]) { 
+	// 	m = l;
+	// }
+
+	if(l < n && arr[l] < arr[m]) { 
+		m = l;
+	}
+
+	// uncomment lines [29-31] and comment lines [33-35] for max heap
+
+	// if(r < n && arr[r] > arr[m]) {
+	// 	m = r;
+	// }
+ 	
+	if(r < n && arr[r] < arr[m]) {
+		m = r;
+	}
+ 	
+ 	// m == i serves as the base case, so we do not need to provide an explicit base case
+	if(m != i) { 
+		swap(arr[i], arr[m]);
+		heapify(arr, n, m);
+	}
+
+}
+
 int main() {
 	
-	int arr[] = {-1, 5, 4, 0, 1, 2, 3}; // 0th index is reserved
+	int arr[] = {-1, 5, 7, 4, 0, 6, 1, 2, 3}; // don't forget, 0th index is reserved
 	int n = sizeof(arr) / sizeof(int);
 
-	for(int i=n/2; i>=1; i--) {
-		heapify(arr, n, i); // do it yourself
+	for(int i=n/2; i>=1; i--) { // 1st non-left node is present at the (n/2)th index
+		heapify(arr, n, i);
 	}
+
+	for(int i=1; i<n; i++) {
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
 
 	return 0;
 }
