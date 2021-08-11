84397eb8b3f12d90163d3d2598376e9b41a9c6c0
abhishek18124 <abhishek18124@iiitd.ac.in>
2021-08-11 21:52:23 +0530

    Lecture 32 DP Class II

--- August/11thAugustLecture32/003DP_CoinChange.cpp
+++ August/11thAugustLecture32/003DP_CoinChange.cpp
@@ -15,18 +15,92 @@ Example 1:
 	Output: 3
 
 	Input: coins = [3, 6], amount = 13
-	Output: 3
+	Output: no way
 
 */
 
 #include<iostream>
 #include<vector>
-=
+#include<climits>
+
 using namespace std;
 
+int minCoinsToZero(vector<int>& coins, int n) {
+	// base case
+	if(n == 0) return 0;
+
+	// recursive case
+	int minCoins = INT_MAX;
+	for(int i=0; i<coins.size(); i++) {
+		if(n-coins[i] >= 0) {
+			minCoins = min(minCoins, minCoinsToZero(coins, n-coins[i]));
+
+		}
+	}
+
+	return minCoins == INT_MAX ? minCoins : 1 + minCoins; 
+}
+
+int minCoinsToZeroTopDown(vector<int>& coins, int n, vector<int>& dp) {
+	// base case
+	if(n == 0) return dp[n]=0;
+
+	// lookup
+	if(dp[n] != -1) return dp[n];
+
+	// recursive case
+	int minCoins = INT_MAX;
+	for(int i=0; i<coins.size(); i++) {
+		if(n-coins[i] >= 0) {
+			minCoins = min(minCoins, minCoinsToZeroTopDown(coins, n-coins[i], dp));
+		}
+	}
+
+	int ans = minCoins == INT_MAX ? minCoins : 1 + minCoins;
+	return dp[n] = ans; 
+}
+
+int minCoinsToZeroBottomUp(vector<int>& coins, int n) {
+	vector<int> dp(n+1);
+
+	dp[0] = 0;
+	for(int j=1; j<=n; j++) {
+		// dp[j] = f(j) = min. no. of coins required to reduce an amount j to 0
+		int minCoins = INT_MAX;
+		for(int i=0; i<coins.size(); i++) {
+			if(j-coins[i] >= 0) {
+				minCoins = min(minCoins, dp[j-coins[i]]);
+			}
+		}
+
+		dp[j] = minCoins == INT_MAX ? minCoins : 1 + minCoins;
+	}
+
+	return dp[n];
+
+}
 
 int main() {
 
+	vector<int> coins = {5, 10};
+	int n = 35;
+
+	cout << minCoinsToZero(coins, n) << endl;
+
+	vector<int> dp(n+1, -1);
+	cout << minCoinsToZeroTopDown(coins, n, dp) << endl;
+	cout << minCoinsToZeroBottomUp(coins, n) << endl;
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
--- August/11thAugustLecture32/004DP_WineProblem.cpp
+++ August/11thAugustLecture32/004DP_WineProblem.cpp
@@ -3,7 +3,7 @@
 Given n wines in a row, with integers denoting the cost of each wine respectively. Each year you can sell the 
 first or the last wine in the row. However, the price of wines increases over time. 
 
-Let the initial profits from the wines be [p_1, p_2, ... , p_n]. In the yth year, the profit from 
+Let the initial profits from the wines be [p_0, p_1, ... , p_n-1]. In the yth year, the profit from 
 the kth wine will be y*p_k. 
 
 Find the maximum profit from all the wines.
@@ -19,9 +19,78 @@ Example :
 
 using namespace std;
 
+int maxProfit(vector<int>& wines, int i, int j, int y) {
+	// base case
+	if(i == j) return y*wines[i];
+
+	// recursive case
+
+	// you're in the yth year, and you can sell either the ith wine or the jth wine
+
+	// you sell ith wine
+	int x1 = maxProfit(wines, i+1, j, y+1);
+
+	// you sell jth wine
+	int x2 = maxProfit(wines, i, j-1, y+1);
+
+	return max(y*wines[i]+x1, y*wines[j]+x2);
+
+}
+
+int maxProfitTopDown(vector<int>& wines, int i, int j, int y, vector<vector<int>>& dp) {
+	// base case
+	if(i == j) return dp[i][j]=y*wines[i];
+
+	// lookup
+	if(dp[i][j] != -1) return dp[i][j];
+
+	// recursive case
+
+	// you're in the yth year, and you can sell either the ith wine or the jth wine
+
+	// you sell ith wine
+	int x1 = maxProfitTopDown(wines, i+1, j, y+1, dp);
+
+	// you sell jth wine
+	int x2 = maxProfitTopDown(wines, i, j-1, y+1, dp);
+
+	return dp[i][j]=max(y*wines[i]+x1, y*wines[j]+x2);
+
+}
+
+int maxProfitBottomUp(vector<int>& wines) {
+	int n = wines.size();
+	vector<vector<int>> dp(n, vector<int>(n));
+
+	// fill the main diagonal
+	for(int i=0; i<n; i++) {
+		dp[i][i] = n * wines[i];
+	}
+
+	for(int i=n-2; i>=0; i--) {
+		for(int j=i+1; j<n; j++) {
+			int y = n - (j-i);
+			dp[i][j] = max(wines[i]*y + dp[i+1][j],
+						   wines[j]*y + dp[i][j-1]);
+		}
+	}
+
+	return dp[0][n-1];
+
+}
+
 
 int main() {
 
+	vector<int> wines =  {2, 3, 5, 1, 4};
+	int n = wines.size();
+	
+	cout << maxProfit(wines, 0, n-1, 1) << endl;
+
+	vector<vector<int>> dp(n, vector<int>(n, -1));
+
+	cout << maxProfitTopDown(wines, 0, n-1, 1, dp) << endl;
+	cout << maxProfitBottomUp(wines) << endl;
 	
 	return 0;
 }
