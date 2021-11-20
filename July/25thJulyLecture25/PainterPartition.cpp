08b57a60edbfcef2141cffc251ffff3b392713c3
sarthaktuli7 <85154071+sarthaktuli7@users.noreply.github.com>
2021-11-20 18:15:46 +0530

    Update PainterPartition.cpp

--- July/25thJulyLecture25/PainterPartition.cpp
+++ July/25thJulyLecture25/PainterPartition.cpp
@@ -1,4 +1,14 @@
 /*
+Given K painters to paint N boards where each painter takes 1 unit of time to paint 1 unit of boards i.e. if the length of a particular board is 5, it will take 5 units of time to paint the board. Compute the minimum amount of time to paint all the boards.
+
+Note that:
+
+Every painter can paint only contiguous segments of boards.
+A board can only be painted by 1 painter at maximum.
+
+Input Format
+First line contains K which is the number of painters. Second line contains N which indicates the number of boards. Third line contains N space separated integers representing the length of each board.
+
 
 https://hack.codingblocks.com/app/contests/2340/1057/problem
 
